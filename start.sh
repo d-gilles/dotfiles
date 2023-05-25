e1e6b1269abcf56989070ec996c24ecd047a3384
David Gilles <info@davidgilles.de>
2023-05-25 16:14:23 +0200

    add main install_

--- start.sh
+++ start.sh
@@ -2,5 +2,11 @@
 
 sudo apt-get update
 sudo apt-get install -y zsh
-sudo apt-get install -y pyenv
-sudo apt-get install -y oh-my-zsh
+sudo apt-get install -y pip
+curl https://pyenv.run | bash
+
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
