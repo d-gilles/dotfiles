c90efc3941a7146eeeb892716663a198ba992c62
David Gilles <info@davidgilles.de>
2023-05-26 15:43:53 +0000

    add gh command

--- start.sh
+++ start.sh
@@ -8,6 +8,13 @@ sudo pip install virtualenv
 sudo apt install -y python3.10-venv
 sudo apt install tree
 
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
 
 
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
