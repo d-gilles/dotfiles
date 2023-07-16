2286cbd37c1fd230f5360e2f0cc634a8ccade829
David Gilles <info@davidgilles.de>
2023-07-16 21:42:00 +0200

    automate setup

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get install -y zsh
 sudo apt-get install -y pip
 curl https://pyenv.run | bash
+sudo apt-get install -y tree
 
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
