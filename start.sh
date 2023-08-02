1e43ee770e242df413afafeaaaffb33f67c490b5
David Gilles <info@davidgilles.de>
2023-08-02 14:23:32 +0200

    Refactor start.sh for clarity and efficiency

--- start.sh
+++ start.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-sudo apt-get update
+# Update the package lists for upgrades and new package installations
+sudo apt update
 
-# install basics: zsh,pip,pyenv/virtualenv,ohmyzsh
-sudo apt-get install -y zsh
-sudo apt-get install -y pip
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+# Grouped installations
+sudo apt install -y zsh curl tree python3.10-venv
+
+# Check if pip is installed, if not, install it
+type pip || sudo apt install -y python-pip
+
+# Pyenv and Virtualenv installations
 curl https://pyenv.run | bash
 sudo pip install virtualenv
-sudo apt install -y python3.10-venv
 
-
-# installing additional commands: tree,gh
-sudo apt install tree
-sudo apt-get install -y tree
-
-# set path
+# Setting the PATH for Pyenv
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+
+# Fetch the GPG key for GitHub CLI's official APT repository and add it to the keyring.
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /usr/share/keyrings/githubcli-archive-keyring.gpg > /dev/null
+
+# Grant read permissions on the keyring file.
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+
+# Add GitHub CLI's official APT repository to the system's list of sources.
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+# Update the package list.
+sudo apt update
+
+# Install the GitHub CLI.
+sudo apt install gh -y
 
 # Set up the docker repository
 sudo apt-get install ca-certificates curl gnupg
