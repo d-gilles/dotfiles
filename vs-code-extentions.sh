b6aa44fc838ffc2b58a346063b166b797453731c
David Gilles <info@davidgilles.de>
2023-08-04 12:59:15 +0200

    add script for vs-code extentions

---
+++ vs-code-extentions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install the Docker extension
+code --install-extension ms-azuretools.vscode-docker
+
+# Install the GitHub Copilot extension
+code --install-extension github.copilot
+
+# Install the GitHub Copilot Chat extension
+code --install-extension github.copilot-chat
+
+# Install the HashiCorp Terraform extension
+code --install-extension hashicorp.terraform
+
+# Install the Jupyter extension
+code --install-extension ms-toolsai.jupyter
+
+# Install the Jupyter Cell Tags extension
+code --install-extension lukebond.jupyter-cell-tags
+
+# Install the Markdown Preview GitHub Styling extension
+code --install-extension bierner.markdown-preview-github-styles
+
+# Install the Python extension
+code --install-extension ms-python.python
+
+# Install the Remote Development extension
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
